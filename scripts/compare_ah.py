cea77730f32906c72b83169a2d65f9f0b803cca0
Peter Isaac <pisaac.ozflux@gmail.com>
2014-03-19 07:54:01 +1100

    auto dev server commit

--- scripts/compare_ah.py
+++ scripts/compare_ah.py
@@ -14,15 +14,17 @@ import time
 nfig = 0
 plotwidth = 10.9
 plotheight = 7.5
-
+# load the control file
 cf = qcio.load_controlfile(path='../controlfiles')
 if len(cf)==0: sys.exit()
-
+min_n = int(cf["General"]["minimum_number"])
+min_r = float(cf["General"]["minimum_correlation"])
+# get the input file name
 fname = qcio.get_infilename_from_cf(cf)
 if not os.path.exists(fname):
     print " compare_ah: Input netCDF file "+fname+" doesn't exist"
     sys.exit()
-
+# read the input file and return the data structure
 ds = qcio.nc_read_series(fname)
 if len(ds.series.keys())==0: print time.strftime('%X')+' netCDF file '+fname+' not found'; sys.exit()
 # get the site name
@@ -53,10 +55,6 @@ Ah_7500_name = str(cf['Variables']['Ah_7500'])
 Ah_HMP_name = str(cf['Variables']['Ah_HMP'])
 ah_7500_30min_1d,flag = qcutils.GetSeriesasMA(ds,Ah_7500_name,si=si,ei=ei)
 ah_HMP1_30min_1d,flag = qcutils.GetSeriesasMA(ds,Ah_HMP_name,si=si,ei=ei)
-# reject 7500 data when the difference between the 7500 and HMP absolute humidities
-# is larger than 10% of the HMP value
-#d = numpy.ma.abs(ah_7500_30min_1d - ah_HMP1_30min_1d)
-#ah_7500_30min_1d = numpy.ma.masked_where(d>0.2*ah_HMP1_30min_1d,ah_7500_30min_1d)
 month_30min_1d,flag = qcutils.GetSeriesasMA(ds,'Month',si=si,ei=ei)
 ah_7500_30min_2d = numpy.ma.reshape(ah_7500_30min_1d,[ndays,nperday])
 ah_HMP1_30min_2d = numpy.ma.reshape(ah_HMP1_30min_1d,[ndays,nperday])
@@ -82,91 +80,15 @@ ah_stdratio_daily = ah_HMP1_daily_std/ah_7500_daily_std
 ah_7500range_daily = ah_7500_daily_max - ah_7500_daily_min
 ah_HMP1range_daily = ah_HMP1_daily_max - ah_HMP1_daily_min
 ah_rangeratio_daily = (ah_HMP1_daily_max - ah_HMP1_daily_min)/(ah_7500_daily_max - ah_7500_daily_min)
-
 DT_daily = DateTime[0:nrecs:nperday]
-#Mnth_30min_2d = Mnth_30min_1d.reshape(ndays,nperday)
-
+# time series plot of daily averaged absolute humidities and differencves
 nfig = nfig + 1
 fig = plt.figure(nfig,figsize=(plotwidth,plotheight))
 plt.figtext(0.5,0.95,SiteName,horizontalalignment='center',size=16)
 qcplot.tsplot(DT_daily,ah_7500_daily_avg,sub=[3,1,1],ylabel='Ah_7500')
 qcplot.tsplot(DT_daily,ah_HMP1_daily_avg,sub=[3,1,2],ylabel='Ah_HMP_01')
 qcplot.tsplot(DT_daily,ah_avgdiff_daily,sub=[3,1,3],ylabel='7500-HMP')
-
-#nfig = nfig + 1
-#fig = plt.figure(nfig,figsize=(plotwidth,plotheight))
-#plt.figtext(0.5,0.95,SiteName,horizontalalignment='center',size=16)
-#qcplot.tsplot(DT_daily,ah_7500_daily_max,sub=[6,1,1],ylabel='7500x')
-#qcplot.tsplot(DT_daily,ah_HMP1_daily_max,sub=[6,1,2],ylabel='HMP1x')
-#qcplot.tsplot(DT_daily,ah_7500_daily_min,sub=[6,1,3],ylabel='7500n')
-#qcplot.tsplot(DT_daily,ah_HMP1_daily_min,sub=[6,1,4],ylabel='HMP1n')
-#qcplot.tsplot(DT_daily,ah_7500range_daily,sub=[6,1,5],ylabel='7500r')
-#qcplot.tsplot(DT_daily,ah_HMP1range_daily,sub=[6,1,6],ylabel='HMP1r')
-
-#nfig = nfig + 1
-#fig = plt.figure(nfig,figsize=(plotwidth,plotheight))
-#plt.figtext(0.5,0.95,SiteName,horizontalalignment='center',size=16)
-#qcplot.tsplot(DT_daily,ah_7500_daily_std,sub=[4,1,1],ylabel='Sd(Ah_7500)')
-#qcplot.tsplot(DT_daily,ah_HMP1_daily_std,sub=[4,1,2],ylabel='Sd(Ah_HMP)')
-#qcplot.tsplot(DT_daily,ah_stdratio_daily,sub=[4,1,3],ylabel='Sd(HMP)/Sd(7500)')
-#qcplot.tsplot(DT_daily,ah_rangeratio_daily,sub=[4,1,4],ylabel='HMPr/7500r')
-
-# daily regressions
-slope = numpy.ones(ndays)
-offset = numpy.zeros(ndays)
-correl = numpy.ones(ndays)
-number = numpy.zeros(ndays)
-for i in range(0,ndays-1):
-    x = ah_7500_30min_2d[i,:]
-    y = ah_HMP1_30min_2d[i,:]
-
-    mask = (x.mask)|(y.mask)
-    x.mask = mask
-    y.mask = mask
-    x_nm = numpy.ma.compressed(x)
-    x_nm = sm.add_constant(x_nm)
-    y_nm = numpy.ma.compressed(y)
-    if len(y_nm)>1:
-        resrlm = sm.RLM(y_nm,x_nm,M=sm.robust.norms.TukeyBiweight()).fit()
-        coefs = resrlm.params
-        
-        #coefs = numpy.ma.polyfit(x,y,1)
-        r = numpy.ma.corrcoef(x,y)
-        number[i] = numpy.ma.count(x)
-        slope[i] = coefs[0]
-        offset[i] = coefs[1]
-        correl[i] = r[0][1]
-        #print number[i],slope[i],correl[i]
-
-#nfig = nfig + 1
-#fig = plt.figure(nfig,figsize=(plotwidth,plotheight))
-#plt.figtext(0.5,0.95,SiteName,horizontalalignment='center',size=16)
-#slope2 = numpy.ma.masked_where(correl<0.95,slope)
-#offset2 = numpy.ma.masked_where(correl<0.95,offset)
-#qcplot.tsplot(DT_daily,slope,sub=[5,1,1],ylabel='Slope',colours=correl)
-#qcplot.tsplot(DT_daily,slope2,sub=[5,1,2],ylabel='Slope(r>0.95)',colours=correl)
-#qcplot.tsplot(DT_daily,offset,sub=[5,1,3],ylabel='Offset',colours=correl)
-#qcplot.tsplot(DT_daily,offset2,sub=[5,1,4],ylabel='Offset(r>0.95)',colours=correl)
-#qcplot.tsplot(DT_daily,number,sub=[5,1,5],ylabel='Number',colours=correl)
-
-symbol = ['ro','bo','yo','r+','b+','y+','r*','b*','y*','r^','b^','y^']
-monthlist = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-#nfig = nfig + 1
-#fig = plt.figure(nfig,figsize=(plotwidth,plotheight))
-#plt.figtext(0.5,0.95,SiteName,horizontalalignment='center',size=16)
-##qcplot.xyplot(ah_HMP1_daily_avg,ah_diff_daily_avg,sub=[1,1,1],regr=1,title="Daily Average",xlabel='Ah_HMP (g/m3)',ylabel='7500-HMP (g/m3)')
-#j = 0
-#for i in [1,2,3,4,5,6,7,8,9,10,11,12]:
-    #j = j + 1
-    #index = numpy.where(month_daily_avg==i)[0]
-    #if len(index)!=0:
-        #x = ah_HMP1_daily_avg[index]
-        #y = ah_avgdiff_daily[index]
-        #plt.plot(x,y,symbol[i-1],label=monthlist[i-1])
-        #plt.xlabel('Ah_HMP (g/m3)')
-        #plt.ylabel('Ah_7500 - Ah_HMP (g/m3)')
-    #plt.legend(loc=2,scatterpoints=0,frameon=False)
-
+# scatter plots of absolute humidities by month
 MnthList = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 nfig = nfig + 1
 fig = plt.figure(nfig,figsize=(plotwidth,plotheight))
@@ -188,21 +110,30 @@ for i in [1,2,3,4,5,6,7,8,9,10,11,12]:
             ylabel = 'HMP (g/m3)'
         qcplot.xyplot(x,y,sub=[4,3,j],regr=2,title=MnthList[i-1],xlabel=xlabel,ylabel=ylabel)
 plt.tight_layout()
-
-nfig = nfig + 1
-figts = plt.figure(nfig,figsize=(plotwidth,plotheight))
-plt.figtext(0.5,0.95,SiteName,horizontalalignment='center',size=16)
-qcplot.tsplot(DT_daily,correl,sub=[5,1,1],ylabel='Correl',colours=number)
-qcplot.tsplot(DT_daily,number,sub=[5,1,2],ylabel='Number',colours=correl)
-qcplot.tsplot(DT_daily,slope,sub=[5,1,3],ylabel='Slope',colours=correl)
-qcplot.tsplot(DT_daily,offset,sub=[5,1,4],ylabel='Offset',colours=correl)
-qcplot.tsplot(DT_daily,ah_stdratio_daily,sub=[5,1,5],ylabel='Sd(HMP)/Sd(7500)',colours=correl)
-
-correl2 = numpy.ma.masked_where((correl<0.98)|(number<30),correl)
-number2 = numpy.ma.masked_where((correl<0.98)|(number<30),number)
-slope2 = numpy.ma.masked_where((correl<0.98)|(number<30),slope)
-offset2 = numpy.ma.masked_where((correl<0.98)|(number<30),offset)
-sdratio2 = numpy.ma.masked_where((correl<0.98)|(number<30),ah_stdratio_daily)
+# daily regressions
+slope = numpy.ones(ndays)
+offset = numpy.zeros(ndays)
+correl = numpy.ones(ndays)
+number = numpy.zeros(ndays)
+for i in range(0,ndays-1):
+    x = ah_7500_30min_2d[i,:]
+    y = ah_HMP1_30min_2d[i,:]
+    x_nm = numpy.ma.compressed(x)
+    x_nm = sm.add_constant(x_nm)
+    y_nm = numpy.ma.compressed(y)
+    if len(y_nm)>1:
+        resrlm = sm.RLM(y_nm,x_nm,M=sm.robust.norms.TukeyBiweight()).fit()
+        coefs = resrlm.params
+        r = numpy.ma.corrcoef(x,y)
+        number[i] = numpy.ma.count(x)
+        slope[i] = coefs[0]
+        offset[i] = coefs[1]
+        correl[i] = r[0][1]
+correl2 = numpy.ma.masked_where((correl<min_r)|(number<min_n),correl)
+number2 = numpy.ma.masked_where((correl<min_r)|(number<min_n),number)
+slope2 = numpy.ma.masked_where((correl<min_r)|(number<min_n),slope)
+offset2 = numpy.ma.masked_where((correl<min_r)|(number<min_n),offset)
+sdratio2 = numpy.ma.masked_where((correl<min_r)|(number<min_n),ah_stdratio_daily)
 nfig = nfig + 1
 figts = plt.figure(nfig,figsize=(plotwidth,plotheight))
 plt.figtext(0.5,0.95,SiteName,horizontalalignment='center',size=16)
@@ -275,8 +206,8 @@ class PointBrowser:
         x.mask = mask
         y.mask = mask
         print DateTime[self.ind_start],DateTime[self.ind_30min],numpy.ma.count(x),numpy.ma.corrcoef(x,y)[0][1]
-        if numpy.ma.count(x)>30:
-            if (numpy.ma.corrcoef(x,y)[0][1]>0.98):
+        if numpy.ma.count(x)>min_n:
+            if (numpy.ma.corrcoef(x,y)[0][1]>min_r):
                 x_nm = numpy.ma.compressed(x)
                 y_nm = numpy.ma.compressed(y)
                 self.x_obs = numpy.ma.concatenate([self.x_obs,x_nm])
