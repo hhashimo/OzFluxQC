9c2262bf73d9c5387ff0e48657f79b076e63e647
Peter Isaac <pisaac.ozflux@gmail.com>
2014-09-12 07:34:54 +1000

    Implement new version of InterpolateOverMissing  - added contiguous_regions to qcutils  - began modifying qcts.InterpolateOverMissing

--- scripts/qcutils.py
+++ scripts/qcutils.py
@@ -120,26 +120,26 @@ def CheckTimeStep(ds,mode="fix"):
     return has_gaps
 
 def contiguous_regions(condition):
-    """Finds contiguous True regions of the boolean array "condition". Returns
-    a 2D array where the first column is the start index of the region and the
-    second column is the end index."""
-
+    """
+    Purpose:
+     Finds contiguous True regions of the boolean array "condition". Returns
+     a 2D array where the first column is the start index of the region and the
+     second column is the end index.
+    Author: Joe Kington (via StackOverflow)
+    Date: September 2014
+    """
     # Find the indicies of changes in "condition"
     d = np.diff(condition)
     idx, = d.nonzero() 
-
     # We need to start things after the change in "condition". Therefore, 
     # we'll shift the index by 1 to the right.
     idx += 1
-
     if condition[0]:
         # If the start of condition is True prepend a 0
         idx = np.r_[0, idx]
-
     if condition[-1]:
         # If the end of condition is True, append the length of the array
         idx = np.r_[idx, condition.size] # Edit
-
     # Reshape the result into two columns
     idx.shape = (-1,2)
     return idx
