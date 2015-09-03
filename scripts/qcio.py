3653e3f7021aefcec56b66bb4d66ac4436112276
Peter Isaac <pisaac.ozflux@gmail.com>
2015-09-03 13:49:50 +1000

    Implemented reading of CSV files at L1:  - new routine csv_read_series in qcio.py  - L1 control file needs [[[xl]]] replaced by [[[csv]]] Minor bug fixes in template files.

--- scripts/qcio.py
+++ scripts/qcio.py
@@ -140,6 +140,80 @@ def copy_datastructure(cf,ds_in):
                         pass
     return ds_out
 
+def csv_read_series(cf):
+    """
+    Purpose:
+     Reads a CSV file and returns the data in a data structure.
+     The CSV file must conform to the following rules:
+      1) the first line of the CSV file is a header line that contains
+         the variable names for each column
+      2) the second and all subsequent lines must contain data
+      3) the first colume must contain a string representation
+         of the datetime
+      4) missing data in the CSV file is represented by a blank
+         or by "NA".
+    Usage:
+     ds = csv_read_series(cf)
+     where cf is a control file
+           ds is a data structure
+    Author: PRI
+    Date: September 2015
+    """
+    ds = DataStructure()
+    # get the filename
+    csv_name = get_infilenamefromcf(cf)
+    if len(csv_name)==0:
+        log.error(' in_filename not found in control file')
+        return ds
+    if not os.path.exists(csv_name):
+        log.error(' Input file '+csv_name+' specified in control file not found')
+        return ds
+    # get the header and first data rows
+    FirstDataRow = int(qcutils.get_keyvaluefromcf(cf,["Files"],"in_firstdatarow")) - 1
+    header_row = int(qcutils.get_keyvaluefromcf(cf,["Files"],"in_headerrow")) - 1
+    # get the CSV workbook object
+    log.info(" Opening and reading CSV file "+csv_name)
+    # get the header line from the CSV file
+    csv_file = open(csv_name,'rb')
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    header = csv_reader.next()
+    # construct the dtype list for use in numpy.genfromtxt
+    dtype = [(header[0],'object')]+[(a,'f') for a in header[1:]]    
+    # converter function for datetimes
+    convert_datetime = lambda x: dateutil.parser.parse(x)
+    # read the CSV file
+    data = numpy.genfromtxt(csv_name,delimiter=",",skip_header=0,names=True,converters={0:convert_datetime},
+                            dtype=dtype,missing_values="NA",filling_values=float(-9999))
+    # set some global attributes
+    nRecs = len(data[header[0]])
+    ds.globalattributes["nc_nrecs"] = str(nRecs)
+    ds.globalattributes['featureType'] = 'timeseries'
+    ds.globalattributes['csv_filename'] = csv_name
+    ds.globalattributes['xl_datemode'] = str(0)
+    s = os.stat(csv_name)
+    t = time.localtime(s.st_mtime)
+    ds.globalattributes['csv_moddatetime'] = str(datetime.datetime(t[0],t[1],t[2],t[3],t[4],t[5]))
+    # get the datetime
+    ds.series["DateTime"] = {}
+    ds.series["DateTime"]["Data"] = list(data["TIMESTAMP"])
+    ds.series["DateTime"]["Flag"] = numpy.zeros(nRecs,dtype=numpy.int32)
+    ds.series["DateTime"]["Attr"] = {}
+    ds.series["DateTime"]["Attr"]["long_name"] = "Datetime in local timezone"
+    ds.series["DateTime"]["Attr"]["units"] = "None"
+    # get the variables
+    var_list = cf["Variables"].keys()
+    if "xlDateTime" in var_list: var_list.remove("xlDateTime")
+    for var in var_list:
+        csv_var_name = cf["Variables"][var]["csv"]["name"]
+        csv_var_name = csv_var_name.replace(".","")
+        if csv_var_name not in data.dtype.names:
+            log.warning("Requested variable "+csv_var_name+" not found in CSV file")
+            continue
+        ds.series[var] = {}
+        ds.series[var]["Data"] = data[csv_var_name]
+        ds.series[var]["Flag"] = numpy.zeros(nRecs,dtype=numpy.int32)
+    return ds
+
 def nc_2xls(ncfilename,outputlist=None):
     # read the netCDF file
     ds = nc_read_series(ncfilename)
@@ -395,12 +469,19 @@ def smap_writeheaders(cf,csvfile):
 
 def xl2nc(cf,InLevel):
     # get the data series from the Excel file
-    ds = xl_read_series(cf)
-    if len(ds.series.keys())==0: return 1
+    in_filename = get_infilenamefromcf(cf)
+    file_name,file_extension = os.path.splitext(in_filename)
+    if "csv" in file_extension.lower():
+        ds = csv_read_series(cf)
+        # get a series of Excel datetime from the Python datetime objects
+        qcutils.get_xldatefromdatetime(ds)
+    else:
+        ds = xl_read_series(cf)
+        # get a series of Python datetime objects from the Excel datetime
+        qcutils.get_datetimefromxldate(ds)
+    #if len(ds.series.keys())==0: return 1
     # get the netCDF attributes from the control file
     qcts.do_attributes(cf,ds)
-    # get a series of Python datetime objects from the Excel datetime
-    qcutils.get_datetimefromxldate(ds)
     # round the Python datetime to the nearest second
     qcutils.round_datetime(ds,mode="nearest_second")
     #check for gaps in the Python datetime series and fix if present
